80da5ca48710a3df129ec1658c775b18d57620ce
Connor Wiltshire <cwnz2004@gmail.com>
2024-04-08 10:57:26 +1200

    Setup and Basic Navbar done

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <>
+      <div className="Visvine-Nav-Banner">
+        <h1 className="Visvine-Title">Visvine</h1>
+        <button className="Button" id="Sign-in">
+          Sign in
+        </button>
+      </div>
+    </>
+  )
+}
+export default Navbar
