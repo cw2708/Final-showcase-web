830fb57d4922cea6d9ca77a91eb3831d85a15990
Connor Wiltshire <cwnz2004@gmail.com>
2024-04-09 08:57:19 +1200

    Added Auth0 to sign in and out

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,11 +1,37 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 function Navbar() {
+  const { isAuthenticated, logout, loginWithRedirect } = useAuth0()
+  const auth = useAuth0()
+  // TODO: replace placeholder user object with the one from auth0
+  const user = {
+    nickname: auth.user?.name,
+  }
+
+  const handleSignOut = () => {
+    // console.log('sign out')
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+    console.log(user.nickname)
+    // console.log('sign in'
+  }
+
   return (
     <>
       <div className="Visvine-Nav-Banner">
         <h1 className="Visvine-Title">Visvine</h1>
-        <button className="Button" id="Sign-in">
-          Sign in
-        </button>
+        {isAuthenticated ? (
+          <button className="Button" id="Sign-in-out" onClick={handleSignOut}>
+            Sign out
+          </button>
+        ) : (
+          <button className="Button" id="Sign-in-out" onClick={handleSignIn}>
+            Sign in
+          </button>
+        )}
       </div>
     </>
   )
--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import routes from './routes.tsx'
 
@@ -9,10 +9,20 @@ const router = createBrowserRouter(routes)
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+  const root = createRoot(document.getElementById('app') as HTMLElement)
+  root.render(
+    <Auth0Provider
+      domain="dev-cibd174mgdy1x15w.us.auth0.com"
+      clientId="weMqbR4Qk27hlfWTY0tmIHLe37hTrYcQ"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+      ,
+    </Auth0Provider>,
   )
 })
