c01532eb3fd3acc5e99d907fa856d09a9b8e9462
Connor Wiltshire <cwnz2004@gmail.com>
2024-04-10 11:28:48 +1200

    changed api to api/detection

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
-      '/flask-api': 'http://localhost:5000',
+      '/api/detection': 'http://127.0.0.1:5000',
     },
   },
 })
