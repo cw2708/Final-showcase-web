20913db3bc315ee6aef9325b5fca83a42be50ad1
Connor Wiltshire <cwnz2004@gmail.com>
2024-04-10 16:48:01 +1200

    update server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask 
+from ultralytics import YOLO
+import torch 
+import numpy
+import cv2
+from time import time
 
 app = Flask(__name__)
 
@@ -9,6 +14,25 @@ def index():
 @app.route("/api/detection/members")
 def members():
   return{"members": ["Member1", "Member2", "Member3",]}
+
+@app.route("/api/detection/detect")
+def __init__(self, capture_index):
+   self.capture_index = capture_index
+   self.device = "cpu"
+   self.model = self.load_model()
+
+def load_model(self):
+  model = YOLO('yolov8n.pt')
+  model.fuse()
+
+  return model
+
+def predict(self, frame):
+  result = self.model(frame)
+  
+  return result
+
+def plot_bboxes(self, results, frame):
 
 if __name__ == "__main__":
   app.run(debug=True)
