2312797ba6159f6a1208bf396e90146ebebc939a
Connor Wiltshire <cwnz2004@gmail.com>
2024-04-10 20:56:23 +1200

    can fetch results data

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,8 @@
-from flask import Flask 
+from flask import Flask, jsonify, request
 from ultralytics import YOLO
-import torch 
-import numpy
-import cv2
-from time import time
 
 app = Flask(__name__)
+model = YOLO('yolov8n.pt')
 
 @app.route("/api/detection")
 def index():
@@ -16,23 +13,28 @@ def members():
   return{"members": ["Member1", "Member2", "Member3",]}
 
 @app.route("/api/detection/detect")
-def __init__(self, capture_index):
-   self.capture_index = capture_index
-   self.device = "cpu"
-   self.model = self.load_model()
+def predict():
+    # if 'image' not in request.files:
+    #     return jsonify({'error': 'No image provided'}), 400
 
-def load_model(self):
-  model = YOLO('yolov8n.pt')
-  model.fuse()
+    # image = request.files['image']
+    # image_bytes = image.read()
+    # image_tensor = torch.from_numpy(image_bytes).permute(2, 0, 1).float() / 255.0
+    # results = model(image_tensor, size=640)
+    results = model('https://ultralytics.com/images/bus.jpg')
 
-  return model
+    detections = []
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            conf, cls = box.conf[0].item(), box.cls[0].item()
+            detections.append({
+                'class_id': int(cls),
+                'confidence': float(conf),
+                'bounding_box': [x1, y1, x2, y2]
+            })
 
-def predict(self, frame):
-  result = self.model(frame)
-  
-  return result
-
-def plot_bboxes(self, results, frame):
+    return jsonify({'detections': detections})
 
 if __name__ == "__main__":
   app.run(debug=True)
